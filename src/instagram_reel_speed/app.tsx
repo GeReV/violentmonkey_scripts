6264c5b1e1bffea2a9a70a243da70298afa29730
Amir Grozki <amirgrozki@gmail.com>
2025-03-15 03:46:44 +0200

    Added a script for an Instagram reels speed control.

---
+++ src/instagram_reel_speed/app.tsx
@@ -0,0 +1,131 @@
+import { For, render } from 'solid-js/web';
+import { classNames, getPanel, IPanelResult } from '@violentmonkey/ui';
+import { onNavigate } from '@violentmonkey/url';
+// global CSS
+import globalCss from './style.css';
+// CSS modules
+import styles, { stylesheet } from './style.module.css';
+import { createEffect, createSignal } from 'solid-js';
+import { debounce } from '../utils';
+import { observe } from '@violentmonkey/dom';
+
+const ACCOUNT_REEL_URL_REGEX = /^\/\w+\/reel\//;
+
+function smallDiff(a: number, b: number) {
+  return Math.abs(a - b) < 1e-6;
+}
+
+const [getVid, setVid] = createSignal<HTMLVideoElement | null>(null);
+
+function SpeedControl() {
+  const [getSpeed, setSpeed] = createSignal(1);
+
+  // Set video's playbackRate when speed or video changes.
+  createEffect(() => {
+    const vid = getVid();
+    const speed = getSpeed();
+
+    // Use an idle callback to apply the change, because otherwise it doesn't seem to work.
+    // Seems to still miss occasionally.
+    // Possibly Instagram code sets speed right before play?
+    requestIdleCallback(() => {
+      if (vid) {
+        vid.playbackRate = speed;
+      }
+    });
+  });
+
+  const handleClick = (speed: number, _ev: MouseEvent) => {
+    setSpeed(speed);
+  };
+
+  return (
+    <div class={styles.container}>
+      <For each={[1, 1.5, 1.75, 2]}>
+        {(item) => (
+          <button
+            class={classNames([
+              styles.btn,
+              smallDiff(getSpeed(), item) && styles.btn_selected,
+            ])}
+            onclick={[handleClick, item]}
+          >
+            &times;{item}
+          </button>
+        )}
+      </For>
+      <span>&times;{getSpeed()}</span>
+    </div>
+  );
+}
+
+function isInstagramSingleMediaViewPath(pathname: string) {
+  return (
+    pathname.startsWith('/p/') ||
+    pathname.startsWith('/reel/') ||
+    ACCOUNT_REEL_URL_REGEX.test(pathname)
+  );
+}
+
+function positionControl(vid: HTMLVideoElement, panel: IPanelResult) {
+  const rect = vid.getBoundingClientRect();
+
+  panel.wrapper.style.top = rect.bottom + 'px';
+  panel.wrapper.style.left = rect.left + 'px';
+}
+
+function reset(panel: IPanelResult) {
+  if (!isInstagramSingleMediaViewPath(window.location.pathname)) {
+    panel.hide();
+    return;
+  }
+
+  observe(document.body, () => {
+    const vid = document.querySelector<HTMLVideoElement>(
+      '[role="dialog"] video, video',
+    );
+
+    if (!vid) {
+      panel.hide();
+      return;
+    }
+
+    requestIdleCallback(() => {
+      positionControl(vid, panel);
+
+      setVid(vid);
+
+      panel.show();
+    });
+
+    // We found a video, can stop observing.
+    return true;
+  });
+}
+
+function init() {
+  const panel = getPanel({
+    theme: 'dark',
+    style: [globalCss, stylesheet].join('\n'),
+  });
+  panel.setMovable(true);
+
+  const resetPanel = () => reset(panel);
+
+  render(() => <SpeedControl />, panel.body);
+
+  const debouncedPositionControl = debounce(() => {
+    const vid = getVid();
+
+    if (vid) {
+      positionControl(vid, panel);
+    }
+  }, 100);
+  window.addEventListener('resize', debouncedPositionControl, false);
+
+  window.addEventListener('popstate', resetPanel, false);
+  onNavigate(resetPanel);
+  resetPanel();
+}
+
+init();
---
+++ src/instagram_reel_speed/meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Instragram Reel Speed Control
+// @namespace   Grozki Scripts
+// @description This is a userscript.
+// @match       *://www.instagram.com/*
+// @grant       GM_addStyle
+// @version     1.1.0
+// @author      process.env.AUTHOR
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/url@0.1
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/ui@0.7
+// @require     https://cdn.jsdelivr.net/npm/@violentmonkey/dom@2/dist/solid.min.js
+// ==/UserScript==
+
+/**
+ * Code here will be ignored on compilation. So it's a good place to leave messages to developers.
+ *
+ * - The `@grant`s used in your source code will be added automatically by `rollup-plugin-userscript`.
+ *   However you have to add explicitly those used in required resources.
+ * - `process.env.AUTHOR` will be loaded from `package.json`.
+ */
